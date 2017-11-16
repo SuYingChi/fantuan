88e55611239f325389bb5412f4f061185db1517b
zhoukang <zhoukang@yuxun.com.cn>
2017-11-16 10:03:45 +0800

    update

--- app/src/main/java/com/wetime/fanc/web/WebActivity.java
+++ app/src/main/java/com/wetime/fanc/web/WebActivity.java
@@ -13,8 +13,6 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
-import com.king.batterytest.fbaselib.LogoutEvent;
-import com.king.batterytest.fbaselib.customview.multiimageselector.MultiImageSelectorActivity;
 import com.king.batterytest.fbaselib.main.BaseActivity;
 import com.king.batterytest.fbaselib.utils.GsonUtils;
 import com.king.batterytest.fbaselib.utils.Tools;
@@ -23,20 +21,13 @@ import com.wetime.fanc.R;
 import com.wetime.fanc.login.act.LoginActivity;
 import com.wetime.fanc.main.bean.PostFileResultBean;
 import com.wetime.fanc.main.ivews.IPostMultiFileView;
-import com.wetime.fanc.main.presenter.PostMultiFilePresenter;
-
-import org.greenrobot.eventbus.EventBus;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-import static com.king.batterytest.fbaselib.utils.Tools.REQUEST_IMAGE;
 
-
-public class WebActivity extends BaseActivity implements IPostMultiFileView {
+public class WebActivity extends BaseActivity {
 
     @BindView(R.id.tv_title)
     TextView tvTitle;
@@ -48,7 +39,7 @@ public class WebActivity extends BaseActivity implements IPostMultiFileView {
     private String weburl;
     private String type = "0";
 
-    private ArrayList<String> defaultDataArray = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,20 +90,6 @@ public class WebActivity extends BaseActivity implements IPostMultiFileView {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE) {
-            if (resultCode == RESULT_OK) {
-                // 获取返回的图片列表
-                defaultDataArray = data
-                        .getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
-
-            }
-            new PostMultiFilePresenter(this).PostMultiFile(defaultDataArray);
-        }
-    }
 
     @SuppressLint("JavascriptInterface")
     private void webSetting() {
@@ -142,9 +119,8 @@ public class WebActivity extends BaseActivity implements IPostMultiFileView {
         setting.setDomStorageEnabled(true);
         setting.setAppCacheEnabled(true);
         setting.setLoadWithOverviewMode(true);
-
         setting.setDatabaseEnabled(true);
-        web.addJavascriptInterface(this, "android");
+
 
         web.setWebChromeClient(wvcc);
         web.setWebViewClient(new WebViewClient() {
@@ -183,33 +159,10 @@ public class WebActivity extends BaseActivity implements IPostMultiFileView {
     }
 
 
-    @Override
-    public void onPostResult(PostFileResultBean bean) {
-        String ss = GsonUtils.getGsonInstance().toJson(bean);
-        web.loadUrl("javascript:returnPicData('" + ss + "');");
-    }
 
 
-    private void gotoSelectPic() {
-        Intent intent = new Intent(mContext, MultiImageSelectorActivity.class);
-        // 是否显示调用相机拍照
-        intent.putExtra(MultiImageSelectorActivity.EXTRA_SHOW_CAMERA, true);
-        // 最大图片选择数量
-        intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_COUNT, getResources().getInteger(R.integer.most_pic_num));
-        // 设置模式 (支持 单选/MultiImageSelectorActivity.MODE_SINGLE 或者
-        // 多选/MultiImageSelectorActivity.MODE_MULTI)
-        intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_MODE,
-                MultiImageSelectorActivity.MODE_MULTI);
-        // 默认选择图片,回填选项(支持String ArrayList)
-        // ArrayList<String> temp = new ArrayList<String>();
-        // for (int i = 0; i < defaultDataArray.size(); i++) {
-        // temp.add(defaultDataArray.get(i));
-        // }
-        intent.putStringArrayListExtra(
-                MultiImageSelectorActivity.EXTRA_DEFAULT_SELECTED_LIST,
-                defaultDataArray);
-        startActivityForResult(intent, REQUEST_IMAGE);
-    }
+
+
 
     @JavascriptInterface
     public String getToken() {
@@ -226,51 +179,8 @@ public class WebActivity extends BaseActivity implements IPostMultiFileView {
         tvTitle.setText(title);
     }
 
-    @JavascriptInterface
-    public void goSelectPic() {
-        defaultDataArray.clear();
-        gotoSelectPic();
-    }
 
-    @JavascriptInterface
-    public void logout() {
-        Tools.logout(this);
-        finish();
-        EventBus.getDefault().post(new LogoutEvent());
-    }
 
-    @JavascriptInterface
-    public void showLoading() {
-        Tools.showWaitDialog(this);
-    }
-
-    @JavascriptInterface
-    public void hideLoading() {
-        Tools.hideWaitDialog();
-    }
-
-    @JavascriptInterface
-    public void hideRightButton() {
-        web.post(new Runnable() {
-            @Override
-            public void run() {
-                tvRight.setVisibility(View.GONE);
-            }
-        });
-
-    }
-
-    @JavascriptInterface
-    public void setRightButtonText(final String text) {
-        web.post(new Runnable() {
-            @Override
-            public void run() {
-                tvRight.setText(text);
-                tvRight.setVisibility(View.VISIBLE);
-            }
-        });
-
-    }
 }
 
 
