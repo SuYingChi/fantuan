218fe08b2679bc099efcbf05601be7a2e5b8e50a
zhoukang <zhoukang@yuxun.com.cn>
2017-12-06 09:26:46 +0800

    UI  change

--- app/src/main/java/com/wetime/fanc/login/act/PSWLoginActivity.java
+++ app/src/main/java/com/wetime/fanc/login/act/PSWLoginActivity.java
@@ -62,6 +62,7 @@ public class PSWLoginActivity extends BaseActivity implements ILoginView {
                     Tools.toastInBottom(this, "请输入正确手机号");
                     return;
                 }
+                Tools.hideSoftInput(this);
                 if (etCode.getText().length() == 0) {
                     Tools.toastInBottom(this, "请输入正确填写密码");
                     return;
