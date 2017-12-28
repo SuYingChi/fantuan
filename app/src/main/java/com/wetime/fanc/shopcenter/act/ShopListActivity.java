0b54672d6ada93a48ff0504d46a0096047241c83
zhoukang <zhoukang@yuxun.com.cn>
2017-12-28 18:25:44 +0800

    详情页 基本完成

--- app/src/main/java/com/wetime/fanc/shopcenter/act/ShopListActivity.java
+++ app/src/main/java/com/wetime/fanc/shopcenter/act/ShopListActivity.java
@@ -21,6 +21,7 @@ import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.wetime.fanc.R;
+import com.wetime.fanc.shop.act.ShopDetailActivity;
 import com.wetime.fanc.shopcenter.adapter.CategoryItemAdapter;
 import com.wetime.fanc.shopcenter.adapter.FloorItemAdapter;
 import com.wetime.fanc.shopcenter.adapter.SCategoruItemAdapter;
@@ -407,7 +408,9 @@ public class ShopListActivity extends BaseActivity implements IGetShopCenterList
 
     @Override
     public void onItemClick(View view, int position) {
-        goWeb(list.get(position).getDetail_url());
+        Intent goShop = new Intent(mContext, ShopDetailActivity.class);
+        goShop.putExtra("mid",list.get(position).getMid());
+        startActivity(goShop);
     }
 
     private void goWeb(String url) {
