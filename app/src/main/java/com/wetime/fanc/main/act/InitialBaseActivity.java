ee181459a2945edbff0a234ef3e8c3f14f0f5c11
chenyixi <376051132@qq.com>
2018-04-16 10:37:26 +0800

    fix bug

---
+++ app/src/main/java/com/wetime/fanc/main/act/InitialBaseActivity.java
@@ -0,0 +1,10 @@
+package com.wetime.fanc.main.act;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/4/13.
+ */
+
+public class InitialBaseActivity extends AppCompatActivity {
+}
