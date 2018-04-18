c6a323ced09d8af89ff65bd31d49dd27391cf163
zhoukang <zhoukang@yuxun.com.cn>
2018-04-18 16:35:33 +0800

    新闻 header 增加缓存

--- app/src/main/java/com/wetime/fanc/utils/SimpleCatchKey.java
+++ app/src/main/java/com/wetime/fanc/utils/SimpleCatchKey.java
@@ -4,4 +4,5 @@ import com.wetime.fanc.application.FApp;
 
 public interface SimpleCatchKey {
     String catchkey_home = "catch_homepage_" + Tools.getVerCode(FApp.getInstance());
+    String catchkey_news_header = "catch_news_homeheader" + Tools.getVerCode(FApp.getInstance());
 }
