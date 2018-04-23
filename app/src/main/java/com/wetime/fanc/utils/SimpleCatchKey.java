b8d31d588f5d75d0636148c6d4085c85825b33cf
zhoukang <zhoukang@yuxun.com.cn>
2018-04-23 10:49:33 +0800

    圈子增加缓存

--- app/src/main/java/com/wetime/fanc/utils/SimpleCatchKey.java
+++ app/src/main/java/com/wetime/fanc/utils/SimpleCatchKey.java
@@ -6,4 +6,5 @@ public interface SimpleCatchKey {
     String catchkey_home = "catch_homepage_" + Tools.getVerCode(FApp.getInstance());
     String catchkey_news = "catch_news_" + Tools.getVerCode(FApp.getInstance());
     String catchkey_news_header = "catch_news_homeheader_" + Tools.getVerCode(FApp.getInstance());
+    String catchkey_circle_home = "catch_circle_home_" + Tools.getVerCode(FApp.getInstance());
 }
