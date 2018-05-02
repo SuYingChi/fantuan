32a8f93629392021b45beb772876f5584d63aa16
zhoukang <zhoukang@yuxun.com.cn>
2018-05-02 17:52:14 +0800

    weex 搭建

--- app/src/main/java/com/wetime/fanc/weex/EmptyWeexActivity.java
+++ app/src/main/java/com/wetime/fanc/weex/EmptyWeexActivity.java
@@ -8,6 +8,7 @@ public class EmptyWeexActivity extends WeexBaseActivity {
         super.onCreate(savedInstanceState);
 //        loadWeexPageFromUrl("http://h5.m.taobao.com/js/src/weexlist.js");
 //        weex compile src/foo.vue 目录
+
         loadWeexPageFromUrl("http://192.168.11.139:8081/dist/fan.js");
     }
 }
