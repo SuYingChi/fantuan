b0bf323914b964c1c91d869459b781624cacc320
suyingchi <821649398@qq.com>
2018-05-03 14:01:42 +0800

    login

--- app/src/main/java/com/wetime/fanc/login/iviews/IValidateCodeView.java
+++ app/src/main/java/com/wetime/fanc/login/iviews/IValidateCodeView.java
@@ -8,6 +8,6 @@ import com.wetime.fanc.login.bean.LoginResultBean;
  * Created by zhoukang on 2017/5/19.
  */
 
-public interface IInvalidCodeView extends IBaseVIew {
-    void onInvalidResult(LoginResultBean bean);
+public interface IValidateCodeView extends IBaseVIew {
+    void onResult(LoginResultBean bean);
 }
