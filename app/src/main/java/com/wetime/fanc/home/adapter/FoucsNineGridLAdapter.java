b3e99dd6d035fd5a028ac9cf7d6b50483cd27144
zhoukang <zhoukang@yuxun.com.cn>
2018-05-14 10:45:55 +0800

    首页 关注 长文  四宫格 九宫格

---
+++ app/src/main/java/com/wetime/fanc/home/adapter/FoucsNineGridLAdapter.java
@@ -0,0 +1,79 @@
+package com.wetime.fanc.home.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.wetime.fanc.R;
+import com.wetime.fanc.home.bean.Cover;
+import com.wetime.fanc.home.bean.HomeItemBeanV2;
+import com.wetime.fanc.utils.Tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoucsNineGridLAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<HomeItemBeanV2.CoversBean> mlist = new ArrayList<>();
+    private LayoutInflater inflater = null;
+//    private final int mGridWidth;
+
+    public FoucsNineGridLAdapter(Context mContext, List<HomeItemBeanV2.CoversBean> list) {
+        this.mContext = mContext;
+        this.mlist = list;
+        inflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        return mlist != null ? mlist.size() : 0;
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        final Holder holder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.img_s_item_focus, null);
+            holder = new Holder();
+            holder.iv = convertView.findViewById(R.id.img);
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+
+        Glide.with(mContext.getApplicationContext()).load(mlist.get(position).getCompress())
+                .apply(new RequestOptions()
+                        .centerCrop())
+                .into(holder.iv);
+
+
+        return convertView;
+    }
+
+    private class Holder {
+        private ImageView iv;
+
+    }
+
+
+}
--- app/src/main/java/com/wetime/fanc/home/adapter/HomeFocusAdapterV2.java
+++ app/src/main/java/com/wetime/fanc/home/adapter/HomeFocusAdapterV2.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.wetime.fanc.R;
+import com.wetime.fanc.customview.CanDoBlankGridView;
+import com.wetime.fanc.customview.MixtureTextView;
 import com.wetime.fanc.home.bean.HomeItemBeanV2;
 import com.wetime.fanc.utils.Tools;
 
@@ -45,6 +47,12 @@ public class HomeFocusAdapterV2 extends RecyclerView.Adapter {
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == 10 || viewType == 11) {
             return new V10ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_home_act_type10_v2, parent, false));
+        }
+        if (viewType == 18) {
+            return new V18ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_home_act_type18_v2, parent, false));
+        }
+        if (viewType == 14 || viewType == 19) {
+            return new V19ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_home_act_type19_v2, parent, false));
         }
 
         return new NewsHolderTemp(LayoutInflater.from(mContext).inflate(R.layout.item_news_type_temp, parent, false));
@@ -97,6 +105,8 @@ public class HomeFocusAdapterV2 extends RecyclerView.Adapter {
             } else {
                 mHolder.tvCommentNum.setText(bean.getComment_num());
             }
+
+
             int sw = Tools.getScreenW(mContext);
             //图片 1图
             if (bean.getCovers().size() == 0) {
@@ -140,6 +150,112 @@ public class HomeFocusAdapterV2 extends RecyclerView.Adapter {
                                 .placeholder(R.drawable.iv_default))
                         .into(mHolder.ivCover);
             }
+
+        }
+        if (holder instanceof V19ViewHolder) {
+            V19ViewHolder mHolder = (V19ViewHolder) holder;
+            Glide.with(mContext).load(bean.getAvatar()).into(mHolder.ivHead);
+            mHolder.tvName.setText(bean.getUsername());
+            mHolder.tvTime.setText(bean.getTime());
+            if (TextUtils.isEmpty(bean.getCircle_name())) {
+                mHolder.llHeadCircle.setVisibility(View.GONE);
+            } else {
+                mHolder.llHeadCircle.setVisibility(View.VISIBLE);
+                mHolder.tvCirclename.setText(bean.getCircle_name());
+            }
+            if (!TextUtils.isEmpty(bean.getContent())) {
+                mHolder.tvContent.setVisibility(View.VISIBLE);
+                mHolder.tvContent.setText(bean.getContent());
+            } else {
+                mHolder.tvContent.setVisibility(View.GONE);
+                mHolder.tvContent.setText(bean.getContent());
+            }
+
+            if (TextUtils.isEmpty(bean.getLocation())) {
+                mHolder.tvLoc.setVisibility(View.GONE);
+            } else {
+                mHolder.tvLoc.setVisibility(View.VISIBLE);
+                mHolder.tvLoc.setText(bean.getLocation());
+            }
+            if (bean.getLike_num().equals("0")) {
+                mHolder.tvZannum.setText("点赞");
+            } else {
+                mHolder.tvZannum.setText(bean.getLike_num());
+            }
+            mHolder.tvZannum.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Tools.toastInBottom(mContext, "zan le ");
+                }
+            });
+            if (bean.getComment_num().equals("0")) {
+                mHolder.tvCommentNum.setText("评论");
+            } else {
+                mHolder.tvCommentNum.setText(bean.getComment_num());
+            }
+
+
+            int sw = Tools.getScreenW(mContext);
+            ViewGroup.LayoutParams params = mHolder.gv.getLayoutParams();
+            int iW = sw - (Tools.dip2px(mContext, 15 + 15 + 3 + 3)) / 3;
+            //四宫格
+            if (getItemViewType(position) == 14) {
+                params.width = iW * 2 + Tools.dip2px(mContext, 3);
+                params.height = iW * 2 + Tools.dip2px(mContext, 3);
+                mHolder.gv.setNumColumns(2);
+
+            } else {//九宫格
+                params.width = iW * 3 + Tools.dip2px(mContext, 3 + 3);
+                params.height = iW * 2 + Tools.dip2px(mContext, 3);
+                mHolder.gv.setNumColumns(3);
+            }
+            FoucsNineGridLAdapter adapter = new FoucsNineGridLAdapter(mContext,bean.getCovers());
+            mHolder.gv.setLayoutParams(params);
+            mHolder.gv.setAdapter(adapter);
+
+        }
+
+        if (holder instanceof V18ViewHolder) {
+            V18ViewHolder mHolder = (V18ViewHolder) holder;
+            Glide.with(mContext).load(bean.getAvatar()).into(mHolder.ivHead);
+            mHolder.tvName.setText(bean.getUsername());
+            mHolder.tvTime.setText(bean.getTime());
+            if (TextUtils.isEmpty(bean.getCircle_name())) {
+                mHolder.llHeadCircle.setVisibility(View.GONE);
+            } else {
+                mHolder.llHeadCircle.setVisibility(View.VISIBLE);
+                mHolder.tvCirclename.setText(bean.getCircle_name());
+            }
+            mHolder.tvContent.setText(" " + bean.getTitle());
+
+            if (TextUtils.isEmpty(bean.getLocation())) {
+                mHolder.tvLoc.setVisibility(View.GONE);
+            } else {
+                mHolder.tvLoc.setVisibility(View.VISIBLE);
+                mHolder.tvLoc.setText(bean.getLocation());
+            }
+            if (bean.getLike_num().equals("0")) {
+                mHolder.tvZannum.setText("点赞");
+            } else {
+                mHolder.tvZannum.setText(bean.getLike_num());
+            }
+            mHolder.tvZannum.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Tools.toastInBottom(mContext, "zan le ");
+                }
+            });
+            if (bean.getComment_num().equals("0")) {
+                mHolder.tvCommentNum.setText("评论");
+            } else {
+                mHolder.tvCommentNum.setText(bean.getComment_num());
+            }
+
+            //右侧feng'm封面图
+            if (bean.getCovers().size() > 0) {
+                Glide.with(mContext).load(bean.getCovers().get(0).getCompress()).into(mHolder.ivCover);
+            }
+
 
         }
     }
@@ -194,6 +310,67 @@ public class HomeFocusAdapterV2 extends RecyclerView.Adapter {
 
 
         V10ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    class V19ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_head)
+        CircleImageView ivHead;
+        @BindView(R.id.tv_name)
+        TextView tvName;
+        @BindView(R.id.tv_time)
+        TextView tvTime;
+        @BindView(R.id.tv_circlename)
+        TextView tvCirclename;
+        @BindView(R.id.ll_head_circle)
+        LinearLayout llHeadCircle;
+        @BindView(R.id.tv_zannum)
+        TextView tvZannum;
+        @BindView(R.id.tv_content)
+        TextView tvContent;
+        @BindView(R.id.tv_loc)
+        TextView tvLoc;
+        @BindView(R.id.tv_commentnum)
+        TextView tvCommentNum;
+        @BindView(R.id.gv)
+        CanDoBlankGridView gv;
+
+        @BindView(R.id.rl_iv)
+        RelativeLayout rlIv;
+
+
+        V19ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    class V18ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_head)
+        CircleImageView ivHead;
+        @BindView(R.id.tv_name)
+        TextView tvName;
+        @BindView(R.id.tv_time)
+        TextView tvTime;
+        @BindView(R.id.tv_circlename)
+        TextView tvCirclename;
+        @BindView(R.id.ll_head_circle)
+        LinearLayout llHeadCircle;
+        @BindView(R.id.tv_zannum)
+        TextView tvZannum;
+        @BindView(R.id.tv_content)
+        MixtureTextView tvContent;
+        @BindView(R.id.tv_loc)
+        TextView tvLoc;
+        @BindView(R.id.tv_commentnum)
+        TextView tvCommentNum;
+        @BindView(R.id.iv_cover)
+        ImageView ivCover;
+
+
+        V18ViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
         }
